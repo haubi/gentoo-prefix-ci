85e74ef80db352df69559cd0e7dd15f9ff99b0d2
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2019-03-15 14:40:32 +0100

    wait for cygwin setup.exe to exit

--- cygwin/setup.ps1
+++ cygwin/setup.ps1
@@ -2,11 +2,11 @@ Set-Location -Path $env:BUILD_BINARIESDIRECTORY
 
 Invoke-WebRequest -Uri https://cygwin.com/setup-x86_64.exe -Proxy $env:AGENT_PROXYURL -OutFile setup-x86_64.exe
 
-.\setup-x86_64.exe --version
+.\setup-x86_64.exe --version | Out-Default
 
 $cygroot = $env:BUILD_ARTIFACTSTAGINGDIRECTORY
 
 $proxyarg = ""
 if ( $env:AGENT_PROXYURL ) { $proxyarg = "--proxy $env:AGENT_PROXYURL" }
 
-.\setup-x86_64.exe --arch x86_64 --delete-orphans --force-current --no-admin --no-desktop --no-replaceonreboot --no-shortcuts --no-startmenu --no-version-check --only-site --site http://mirror.easyname.at/cygwin/ --quiet-mode $proxyarg --local-package-dir $env:AGENT_WORKFOLDER --root $cygroot --upgrade-also --verbose --packages wget,gcc-g++,rsync
+.\setup-x86_64.exe --arch x86_64 --delete-orphans --force-current --no-admin --no-desktop --no-replaceonreboot --no-shortcuts --no-startmenu --no-version-check --only-site --site http://mirror.easyname.at/cygwin/ --quiet-mode $proxyarg --local-package-dir $env:AGENT_WORKFOLDER --root $cygroot --upgrade-also --verbose --packages 'wget,gcc-g++,rsync' | Out-Default

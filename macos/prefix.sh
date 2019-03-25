b5ee9c4f80dba160759ab5b5dc7cb73d4f194255
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2019-03-25 09:40:59 +0100

    stop trying to provide /usr/bin/wget

--- macos/prefix.sh
+++ macos/prefix.sh
@@ -3,7 +3,5 @@
 set -ex
 
 sudo xcode-select -s /Library/Developer/CommandLineTools
-ls -ld /usr/bin
-sudo ln -s /usr/local/bin/wget /usr/bin/wget
 
 "${BUILD_SOURCESDIRECTORY}"/prefix/staging-bootstrap.sh --sources="${BUILD_SOURCESDIRECTORY}" --staging="${BUILD_STAGINGDIRECTORY}"

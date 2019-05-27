481651ae7cc3777b8914adb1b6cae9185d91a521
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2019-05-27 11:44:32 +0200

    macOS: run xcode-select --install if necessary

--- macos/prefix.sh
+++ macos/prefix.sh
@@ -2,6 +2,7 @@
 
 set -ex
 
+[[ -d /usr/include ]] || sudo xcode-select --install
 sudo xcode-select -s /Library/Developer/CommandLineTools
 
 "${BUILD_SOURCESDIRECTORY}"/prefix/staging-bootstrap.sh --sources="${BUILD_SOURCESDIRECTORY}" --staging="${BUILD_STAGINGDIRECTORY}"

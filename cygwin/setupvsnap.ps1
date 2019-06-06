cd762323dfe49175a37af52989d09acbb0483198
Michael Haubenwallner <michael.haubenwallner@ssi-schaefer.com>
2019-06-06 10:25:50 +0200

    fetch separate snapshot cygwin1.dll for simpler unpack

--- cygwin/setupvsnap.ps1
+++ cygwin/setupvsnap.ps1
@@ -7,7 +7,8 @@ Invoke-Expression -Command ${PSScriptRoot}/setupv3.ps1
 
 Set-Location -Path "$env:BUILD_ARTIFACTSTAGINGDIRECTORY"
 
-$found = "20190603"
+$found = "none"
+$oldest = "20190603"
 
 $today = Get-Date
 
@@ -21,24 +22,33 @@ Do {
         $MM = $month.toString("00")
         Do {
             $DD = $day.toString("00")
-            Write-Output "trying $YYYY$MM$DD" | Out-Default
+            Write-Output "trying $YYYY$MM$DD snapshot ..." | Out-Default
             try {
-                $result = Invoke-WebRequest -UseBasicParsing -Proxy $env:AGENT_PROXYURL -Uri "https://cygwin.com/snapshots/x86_64/cygwin-$YYYY$MM$DD.tar.xz" -OutFile "cygwin-$YYYY$MM$DD.tar.xz"
+                Invoke-WebRequest -UseBasicParsing -Proxy $env:AGENT_PROXYURL -Uri "https://cygwin.com/snapshots/x86_64/cygwin1-$YYYY$MM$DD.dll.xz" -OutFile "cygwin1-$YYYY$MM$DD.dll.xz" | Out-Default
+                Invoke-WebRequest -UseBasicParsing -Proxy $env:AGENT_PROXYURL -Uri "https://cygwin.com/snapshots/x86_64/cygwin-$YYYY$MM$DD.tar.xz" -OutFile "cygwin-$YYYY$MM$DD.tar.xz" | Out-Default
+                $oldest = "$YYYY$MM$DD"
+                Write-Output "unpack $YYYY$MM$DD snapshot ..." | Out-Default
+                .\bin\xz.exe -d "cygwin1-$YYYY$MM$DD.dll.xz"
+                .\bin\chmod.exe +x "cygwin1-$YYYY$MM$DD.dll"
+                .\bin\cp.exe "bin/cygwin1.dll" "bin/cygwin1.dll.vanilla"
+                .\bin\mv.exe -f "cygwin1-$YYYY$MM$DD.dll" "bin/cygwin1.dll"
+                Write-Output " using $YYYY$MM$DD snapshot" | Out-Default
                 $found = "$YYYY$MM$DD"
-                Write-Output "using snapshot from $found" | Out-Default
-                .\bin\xz.exe -d -c "cygwin-$found.tar.xz" | .\bin\tar.exe -v -x -f - --strip-components=1 usr/bin/cygwin1.dll
             } catch {
-                Write-Output "no $YYYY$MM$DD snapshot" | Out-Default
             }
 
-            if ("$YYYY$MM$DD" -le $found) { Break }
+            if ("$YYYY$MM$DD" -le $oldest) { Break }
         } While (--$day -ge 1)
-        if ("$YYYY$MM$DD" -le $found) { Break }
+        if ("$YYYY$MM$DD" -le $oldest) { Break }
 
         $day = 31;
     } While (--$month -ge 1)
-    if ("$YYYY$MM$DD" -le $found) { Break }
+    if ("$YYYY$MM$DD" -le $oldest) { Break }
 
     $month = 12;
 } While (--$year -ge 2018)
 
+if ( $found -eq "none" ) {
+    Write-Out "downloading snapshot failed" | Out-Default
+    exit 1
+}
